454747fb08d6f5e7cbef3ee54a8841f0d96dec63
Cydoniam <murad.cydonia@gmail.com>
2022-09-21 23:09:13 +0300

    Third comment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
-
 function App() {
+  const test = "boutique"
+  let name = () => "Timmy"
+  const el1 = <h1>Hello, {test}</h1>
+  const el2 = <h1>Hello, {name()}</h1>
+
   return (
-    <div className="App">
+        <div className="App">
       <header className="App-header">
         <p>
           Hello world!
+          {el1}
+          {el2}
         </p>
       </header>
     </div>
