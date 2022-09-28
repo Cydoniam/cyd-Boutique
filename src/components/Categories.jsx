d23063d5254a39b31020a702c7fcc7fb0af7080a
Cydoniam <murad.cydonia@gmail.com>
2022-09-28 21:58:26 +0300

    Categories render

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,21 @@
+import React from "react";
+
 function Categories() {
+  const [activeIndex, setActiveIndex] = React.useState(0);
+
+  const categories = ["All", "Accessories", "Cosmetics", "Interior", "Brands"];
+
   return (
     <div className="categories">
       <ul>
-        <li className="active">All</li>
-        <li>Accessories</li>
-        <li>Cosmetics</li>
-        <li>Interior</li>
-        <li>Brands</li>
+        {categories.map((value, index) => (
+          <li
+            onClick={() => setActiveIndex(index)}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {value}
+          </li>
+        ))}
       </ul>
     </div>
   );
