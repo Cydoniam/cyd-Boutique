a2d000fbdb924c2fa6c6f43195d5cefbdd45aecc
Cydoniam <murad.cydonia@gmail.com>
2022-10-06 21:31:46 +0300

    Added page on product

---
+++ src/pages/FullProduct.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+
+const FullProduct = () => {
+  const [product, setProduct] = React.useState();
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    async function fetchProduct() {
+      try {
+        const { data } = await axios.get(
+          "https://63370fc865d1e8ef26793518.mockapi.io/products/" + id
+        );
+        setProduct(data);
+      } catch (error) {
+        alert("Product is not find");
+        navigate("/");
+      }
+    }
+
+    fetchProduct();
+  }, []);
+
+  if (!product) {
+    return "Loading...";
+  }
+
+  return (
+    <div className="container">
+      <img src={product.imageUrl} />
+      <h2>{product.name}</h2>
+      <p>{product.title}</p>
+      <h4>{product.price}₽</h4>
+    </div>
+  );
+};
+
+export default FullProduct;
